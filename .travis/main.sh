f5a38325c2c1ef634652e7b71a7ac7cabb4e2d12
FaniD <fani.dimou92@gmail.com>
2019-05-26 18:54:11 +0300

    Update .travis.yml
    Script main.sh will configure Travis machine so that
    it can handle arm executables - qemu instance is needed
    and new entries in binfmt_misc - and some docker updates
    and configurations so that it supports BuildKit and RUN
    --mount - experimental features.

--- .travis/main.sh
+++ .travis/main.sh
@@ -3,22 +3,12 @@
 set -o errexit
 
 main() {
-  setup_dependencies
   update_docker_configuration
+  add_packages_for_arm_architecture
 
   echo "SUCCESS:
   Done! Finished setting up Travis machine.
   "
-}
-
-setup_dependencies() {
-  echo "INFO:
-  Setting up dependencies.
-  "
-
-  sudo apt update -y
-  sudo apt install --only-upgrade docker-ce -y
-  docker info
 }
 
 update_docker_configuration() {
@@ -33,6 +23,16 @@ update_docker_configuration() {
   "max-concurrent-uploads": 50
 }' | sudo tee /etc/docker/daemon.json
   sudo service docker restart
+}
+
+add_packages_for_arm_architecture() {
+  echo "INFO:
+  Add qemu-user package to run a qemu instance in user mode 
+  and binfmt-support to expand the binfmt_misc handler so 
+  that it recognises arm executables and use qemu-arm 
+  entry as the interpreter
+  "
+  sudo apt-get --yes --no-install-recommends install binfmt-support qemu-user-static
 }
 
 main
