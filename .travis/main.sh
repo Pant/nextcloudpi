49ce37ee2872018b5988cf01541321ce673754f2
FaniD <fani.dimou92@gmail.com>
2019-05-30 18:02:23 +0300

    Update .travis.yml and main.sh

--- .travis/main.sh
+++ .travis/main.sh
@@ -15,24 +15,10 @@ update_docker_configuration() {
   echo "INFO:
   Updating docker configuration
   "
-
-  #Enable experimental features
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sudo sh get-docker.sh
   echo $'{\n    "experimental": true\n}' | sudo tee /etc/docker/daemon.json
   sudo service docker restart
-
-  #Get latest version of Docker (18.09) that supports all experimental features
-  #Convenience script not always working
-  #  curl -fsSL https://get.docker.com -o get-docker.sh
-  #  sudo sh get-docker.sh
-#  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-#  sudo apt-get update
-#  sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
-
-  #Checking if Docker version is 18.09
-#  docker version
-#  sudo cat /etc/docker/daemon.json
-#  printenv | grep DOCKER
 }
 
 add_packages_for_arm_architecture() {
