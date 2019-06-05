1294767736e7fee3457e4cbbbe3aaef47f84b74a
FaniD <fani.dimou92@gmail.com>
2019-06-05 17:35:43 +0300

    Update travis.yml and create single_arch_travis.sh
    Cleanup old travis files and create a script for
    easy producing single arch travis files with the
    right tags added at each one.
    Test armhf travis

---
+++ .travis/single_arch_travis.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Which arch would you like to build?"
+echo "x86/armhf/arm64"
+read arch
+
+cp travis_docker.yml travis_${arch}.yml
+
+if [[ "$arch" != "x86" ]]; then
+
+  [[ "$arch" == armhf ]] && sed -i "s/amd64/armhf/g" travis_${arch}.yml && sed -i "s/arch_qemu=x86_64/arch_qemu=arm/" travis_${arch}.yml
+  [[ "$arch" == arm64 ]] && sed -i "s/amd64/arm64v8/g" travis_${arch}.yml && sed -i "s/arch_qemu=x86_64/arch_qemu=aarch64/" travis_${arch}.yml
+
+  sed -i "s/x86/${arch}/g" travis_${arch}.yml
+  sed -i "21s,#,," travis_${arch}.yml
+  sed -i "40s,#,," travis_${arch}.yml
+  sed -i "63s,#,," travis_${arch}.yml
+fi
+
+echo "Your produced travis file is: $PWD/travis_${arch}.yml"
